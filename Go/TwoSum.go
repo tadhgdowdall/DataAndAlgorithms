d7f2d8816ab47fee9f2d822aad89fb02a05d1630
tadhgdowdall <tadhg.jamesdowdall@gmail.com>
2026-01-15 23:41:28 +0000

    hash

--- Go/TwoSum.go
+++ Go/TwoSum.go
@@ -7,7 +7,7 @@ func main(){
 }
 
 func TwoSum(nums []int, target int) []int {
-	seen := make(map[int]int)
+	seen := make(map[int]int) // Hash map
 	for i, num := range nums {
 		complement := target - num
 		if j, ok := seen[complement]; ok {
