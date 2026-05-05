322ec385fcc63ab7d9e6733b529e1413f0701ce8
tadhgdowdall <tadhg.jamesdowdall@gmail.com>
2026-05-05 19:14:08 +0100

    shortest to character

--- golang/twoPointers.go
+++ golang/twoPointers.go
@@ -59,3 +59,40 @@ func abs(x int) int {
 	}
 	return x
 }
+
+// Input: s = "loveleetcode", c = "e"
+// Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+// Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
+// The closest occurrence of 'e' for index 0 is at index 3, so the distance is abs(0 - 3) = 3.
+// The closest occurrence of 'e' for index 1 is at index 3, so the distance is abs(1 - 3) = 2.
+// For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, but the distance is still the same: abs(4 - 3) == abs(4 - 5) = 1.
+// The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
+
+func shortestToChar(s string, c byte) []int {
+
+	n := len(s)
+
+	answer := make([]int, n)
+
+	// First pass: left to right
+	prev := -n // something far away
+	for i := 0; i < n; i++ {
+		if s[i] == c {
+			prev = i
+		}
+		answer[i] = i - prev
+	}
+
+	// Second pass: right to left
+	prev = 2 * n // something far away
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == c {
+			prev = i
+		}
+		if prev-i < answer[i] {
+			answer[i] = prev - i
+		}
+	}
+
+	return answer
+}
